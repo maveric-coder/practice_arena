33181a0ae8576f73a2d1f612c5398ba77141d42e
Anand <65268453+anand2909@users.noreply.github.com>
2020-07-05 22:12:03 +0530

    Update Capitalize.py
    https://www.hackerrank.com/challenges/halloween-sale/problem
    
    You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
    
    
    Given a full name, your task is to capitalize the name appropriately.
    
    Input Format
    
    A single line of input containing the full name, .
    
    Constraints
    
    The string consists of alphanumeric characters and spaces.
    Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
    
    Output Format
    
    Print the capitalized string, .
    
    Sample Input
    
    chris alan
    Sample Output
    
    Chris Alan

--- Capitalize.py
+++ Capitalize.py
@@ -6,7 +6,7 @@ import random
 import re
 import sys
 
-# Complete the solve function below
+# Complete the solve function below.
 def solve(s):
     for i in s[:].split():
         s=s.replace(i,i.capitalize())
